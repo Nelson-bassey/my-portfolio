a6ae990e30d2020a6d8b358314d544739abd80a0
Nelson-bassey <nbassey148@gmail.com>
2025-06-25 14:26:10 +0100

    worked on form validation

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ links.forEach(link => {
     navLinks.classList.remove('active');
   });
 })
+
+// === this code below is for the slider in my services item ===
+  // const swiper = new Swiper(".mySwiper", {
+  //   slidesPerView: 1,
+  //   spaceBetween: 30,
+  //   loop: true,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   breakpoints: {
+  //     768: {
+  //       slidesPerView: 2,
+  //     },
+  //     1024: {
+  //       slidesPerView: 3,
+  //     },
+  //   }
+  // });
